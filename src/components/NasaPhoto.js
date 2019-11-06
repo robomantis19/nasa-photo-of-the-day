c456237ee2a99fcc4c70c2d83f9ca7c3aa8d6aa3
robomantis19 <freefavors@gmail.com>
2019-11-06 15:26:46 -0500

    initial photo working in NasaPhoto.js

--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,13 +1,23 @@
-import React from 'react'; 
-
-
+import React, {useState, useEffect} from 'react'; 
+import axios from 'axios'; 
+const photoCss = {
+    width: "500px"
+}
 function NasaPhoto(){
-
-
+    const [photo, setPhoto] = useState()
+    const APIkey = "98REzccm6Ml8n54NaB9XPz4R7nADaOI4iEOOlSj9"; 
+    useEffect(() => { 
+        axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APIkey}`)
+            .then(res => { 
+                console.log(res.data);
+                setPhoto(res.data.hdurl);  
+            })
+    },[])
     return (
 
         <div>
             Photo Here!!
+            <img src={photo} style={photoCss}/> 
         </div>
     )
 };
