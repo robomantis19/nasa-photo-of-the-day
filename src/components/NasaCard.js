9c5625c89e2131400943b08815387b6bfefbf7ee
robomantis19 <freefavors@gmail.com>
2019-11-06 16:23:47 -0500

    added all the data to the page and rendering correctly

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,12 +1,17 @@
 import React from 'react'; 
 
-
+const photoCss = {
+    width: "500px"
+}
 function NasaCard(props){
     
     
     return (
         <div>
-            {props.date}
+            <h2>{props.title}</h2>
+            <h2>{props.date}</h2>
+            <img src={props.image} style={photoCss}/> 
+            <p>{props.explanation}</p> 
         </div>
     )
     
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -1,9 +1,7 @@
 import React, {useState, useEffect} from 'react'; 
 import axios from 'axios'; 
 import NasaCard from './NasaCard'; 
-const photoCss = {
-    width: "500px"
-}
+
 function NasaPhoto(){
     const [pic, setPic] = useState([])
     const APIkey = "98REzccm6Ml8n54NaB9XPz4R7nADaOI4iEOOlSj9"; 
@@ -12,9 +10,6 @@ function NasaPhoto(){
             .then(res => { 
                 console.log(res.data);
                 setPic(res.data); 
-                // for (let el  in res.data) { 
-                //     setPhoto(photo => photo += el);
-                // }
                  
             })
             .catch(error => {
@@ -24,13 +19,12 @@ function NasaPhoto(){
     return (
 
         <div>
-            {pic.map((item, index) =>  {
-                return <NasaCard key={index} date={item.date}/>
-                // <NasaCard key={index} date={item.date} explanation={item.explanation} img={item.hdurl} title={item.title} url={item.url}/> 
-                
-            })}
-            
-            {/* <img src={photo} style={photoCss}/>  */}
+            <NasaCard 
+            title = {pic.title}
+            date = {pic.date}
+            image = {pic.hdurl}
+            explanation = {pic.explanation}
+            />
         </div>
     )
 };
