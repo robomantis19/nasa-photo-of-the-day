913a56b4f67110f04587779eb0bb8cc4be898a06
robomantis19 <freefavors@gmail.com>
2019-11-07 23:50:20 -0500

    finished calendar date update, final mvp..

--- src/components/NasaCard.js
+++ src/components/NasaCard.js
@@ -1,6 +1,8 @@
-import React, {useState, useEffect} from 'react'; 
+import React, {useRef, useState, useEffect} from 'react'; 
 // import Datetime from 'react-datetime'; 
 // import moment from 'moment';
+import { TweenMax, TimelineMax,Elastic, Back} from "gsap";
+
 import {
     Card, CardImg, CardText, CardBody,
     CardTitle, CardSubtitle, Button
@@ -8,55 +10,132 @@ import {
   import styled from "styled-components";
 
 
+  
+
 
 function NasaCard(props){
-    console.log("index: ", props.index); 
+    //console.log("index: ", props.index); 
     // const [time, setTime] = useState(); 
     // const handleInput = event => setTime({[event.target.value]: event.target.value})
         // onChange = {handleInput}
     // const DateTime =  <Datetime   locale= 'en' placeholder="YYYY/MM/DD/" dateFormat="YYYY/MM/DD" value={''} timeFormat={false}/>
     // function count() {
         // for(let i=0; i < 7; i++){
-            console.log("index item", props.index)
+            //console.log("index item", props.index)
         // useEffect(() => { 
-            const Stack = styled.div`
+            const Stack = styled(Card)`
                 padding-left: ${props => (`${props.primary}0` +`px`)};
                 zIndex: ${props => `${props.primary}`};
-                height: 20px;
+                height: 400px;
                 background-color: white;
                 background: white;
                 box-shadow: 0 8px 6px -6px black;
             `
+            const Square = styled.div`
+            padding-left: ${props => (`${props.primary}` +`px`)};
+            zIndex: ${props => `${props.primary}`};
+            width: 20px;
+            height: 20px;
+            background-color: green;
+            box-shadow: 0 8px 6px -6px black;
             
+        `
+        
         // },[props.index])
         // }
     // }
+    const [mouseAnimation, setMouseAnimation] = useState();
+    const [clickAnimation, setClickAnimation] = useState();
+    const [tl] = useState(new TimelineMax({ paused: true }));
+    let imgRef = useRef(null);
+    let textRef = useRef(null);
+    let cardRef = useRef(null);
+    useEffect(() => {
+        setMouseAnimation(
+          TweenMax.to(imgRef, 1, {
+            scale: 1.5,
+            filter: "none",
+            ease: Elastic.easeOut.config(1, 0.75)
+          }).pause()
+        );
+        
+    },[])
 
+    
+    
     return (
         <div>
             {/* {DateTime} */}
             <div>
-                <Stack primary={props.index}>
-                    <img src={props.image} style={{width: `50%`, height: `100px`}}/>
-                    <div>
+                {/* <Stack primary={props.index} 
+                
+                
+                onMouseEnter={() => mouseAnimation.play()}
+
+                ref={element => {
+                    cardRef = element;
+                  }}
+                
+                >
+                    <img 
+                    
+
+                    ref={element => {
+                    imgRef = element;
+                    }} 
+                    src={props.image} style={{width: `50%`, height: `300px`}}/>
+                    <div
+                    ref={element => {
+                        textRef = element;
+                      }}
+                    >
                         {props.title}
                     </div>
                     
 
                 </Stack>
-                {/* <Stack primary={props.index}>
+                <div>
+                <Square primary={props.index}/>
+                </div> */}
+                
+
+                
+                {/* <Stack primary={props.index}> */}
                       
-                    <Card style={{zIndex:props.index}} className="card">
-                        <CardImg top width="50%" height="25%" src={props.image} alt="Card image cap" />
+                    <Card 
+                    onMouseEnter={() => mouseAnimation.play()}
+                    onMouseLeave={()=> mouseAnimation.reverse()}
+                
+                    ref={element => {
+                        cardRef = element;
+                      }}
+                    
+                    // style={{zIndex:props.index}} 
+                    className="card">
+                        {/* <CardImg 
+                        
+                        ref={element => {
+                        imgRef = element;
+                        }} 
+                        
+                        top width="50%" height="25%" 
+                        src={props.image} alt="Card image cap" /> */}
                         <CardBody top height="100%">
                         <CardTitle>{props.title}</CardTitle>
                         <CardSubtitle>{props.date}</CardSubtitle>
-                        <img width="100%" src={props.image} alt="Card image cap" />
-                        <CardText height="100%">{props.explanation}</CardText>
+                        <img 
+                        ref={element => {
+                        imgRef = element;
+                        }} 
+                        width="100%" src={props.image} alt="Card image cap" />
+                        <CardText ref={element => {
+                        textRef = element;
+                        }}
+                        height="100%">{props.explanation}</CardText>
                         <Button>Button</Button>
                         </CardBody>
                     </Card>
-                </Stack>    */}
+                {/* </Stack>    */}
             </div>
             
             
--- src/components/NasaPhoto.js
+++ src/components/NasaPhoto.js
@@ -4,7 +4,7 @@ import NasaCard from './NasaCard';
 
 import Datetime from 'react-datetime'; 
 import moment from 'moment';
-
+import styled from "styled-components";
 
 import {
     Carousel,
@@ -13,24 +13,69 @@ import {
     CarouselIndicators,
     CarouselCaption
   } from 'reactstrap';
-  
-  const DateTime =  <Datetime   locale= 'en' placeholder="YYYY/MM/DD/" dateFormat="YYYY/MM/DD" value={''} timeFormat={false}/>
+  var moment1 = require('moment');
 
+  
+//   function dateConv(input){
+//     let x = input.slice(1,3)
+//     switch(x){
+//         case x== 'Oct':
+//             console.log('jan: ', 10);
+//             break
+//         case x== 'Nov':
+//             console.log('jan: ', 11);
+//             break
+//     }
+//   }
+//   dateConv(DateTime.onChange);
 function NasaPhoto(){
     const [pic, setPic] = useState([])
     const [day, setDay] = useState(['2019-10-14'])
     const APIkey = "98REzccm6Ml8n54NaB9XPz4R7nADaOI4iEOOlSj9"; 
+
+    const [calDate, setcalDate] = useState([])
+
+    const [onepic, setOnepic] = useState([]);
+  
+
+
+    const DateTime =  <Datetime  onChange={(val)=> { setcalDate(moment1( val._d.toString().slice(4,15) ).format('YYYY-MM-DD')) }} locale= 'en' placeholder="YYYY/MM/DD/" dateFormat="YYYY/MM/DD" value={''} timeFormat={false}/>
+
+    let theDay = calDate;
+    console.log("date time: ", theDay)
+
+
+    const Bottom=styled.div`
+            width:100%;
+            height: 20%; 
+            background-color: grey;
+            box-shadow: 16px 8px 6px -6px black;
+            margin-top: 20px;
+            border-top: 5px solid #585858; 
+
+        `
+
     useEffect(() => { 
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=${APIkey}&start_date=2019-11-01&end_date=2019-11-07 `)
             .then(res => { 
                 console.log(res.data);
                 setPic(res.data); 
-                 
-            })
-            .catch(error => {
+                console.log('pic', pic);
+            }).catch(error => {
                 console.log("the data was not return", error);
-              });
+              })         
     },[])
+
+    useEffect(() => { 
+        axios.get(`https://api.nasa.gov/planetary/apod?date=${calDate}&api_key=${APIkey} `)
+                .then(response => { 
+                    console.log(response.data);
+                    setOnepic(response.data)
+                    console.log(onepic)
+                }).catch(error => {
+                    console.log("the data was not return", error);
+                  });
+    },[calDate])
     const items = [];
     pic.map((el, index) => { 
         
@@ -74,8 +119,9 @@ function NasaPhoto(){
             style={{width:`20%`}}
             key={item.src}
         >
+            <CarouselCaption className="caption" captionText={item.caption} captionHeader={item.header} />
             <img src={item.src} alt={item.altText} />
-            <CarouselCaption captionText={item.caption} captionHeader={item.header} />
+            
         </CarouselItem>
         );
     });
@@ -96,9 +142,10 @@ function NasaPhoto(){
         <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
         <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
         </Carousel>
+        <Bottom/>
         <div>
             {DateTime}
-            {pic.map((item, index) => {
+            {/* {pic.map((item, index) => {
 
             return (
                 <NasaCard 
@@ -112,8 +159,21 @@ function NasaPhoto(){
                 explanation = {item.explanation}
                 />
 
-            )})}
+            )})} */}
+
+            <NasaCard 
+                key = {onepic.url}
+                // index = {index}
+                day = {onepic.day}
+                // setDay = {setDay}
+                title = {onepic.title}
+                date = {onepic.date}
+                image = {onepic.hdurl}
+                explanation = {onepic.explanation}
+                />
+            
         </div>
+        
     </div>
     );
 }
